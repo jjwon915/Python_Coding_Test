36c9496b4cbafda50654a3d10161f3e4bd83453f
jjwon915 <jjwon915@naver.com>
2021-03-15 23:53:17 +0900

    round함수 오류 수정.

--- Section2/대표값_2.py
+++ Section2/대표값_2.py
@@ -7,8 +7,9 @@ import sys
 n = int(input())
 arr = list(map(int, input().split()))
 
-# 반올림 round함수 사용. 합계 구하기 sum함수 사용.
-avg = round(sum(arr) / n)
+# 반올림 round함수 사용. 파이썬의 round함수는 round_half_even방식 사용. 따라서 avg = (sum(arr)/n) + 0.5를 한 후 int형으로 형변환해 소수점아래를 없앤다. 
+# 합계 구하기 sum함수 사용.
+avg = int((sum(arr)/n) + 0.5)
 min = 2147000000
 
 # enumerate : list의 index값과 value값을 쌍으로 대응해줌.
